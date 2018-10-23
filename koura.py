257e82e665654f3d776c582654b35c5eb29b1aac
Mari Kekkonen <40823748+MariKekkonen@users.noreply.github.com>
2018-10-23 17:57:37 +0300

    Update koura.py

--- koura.py
+++ koura.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #Mari Kekkonen/Robots of Ni
-#kolmatta tehtävää varten tehty koodi esineisiin tarttumista ja siirtämistä varten; jäi kesken 
+#Python code created during Robot Uprising 2018 hackathon, Helsinki, Finland 
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.motor import MediumMotor, OUTPUT_D
